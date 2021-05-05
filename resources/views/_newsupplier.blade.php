6fccb5c9168ef989ce94f8bbb9eb73815d659d27
Vaghani Vivek <77238899+vaghanivivek@users.noreply.github.com>
2021-05-05 03:02:22 -0700

    try3

--- resources/views/_newsupplier.blade.php
+++ resources/views/_newsupplier.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <!--
-This is a starter template page. Use this page to start your new project from
+This is a lavistarter template page. Use this page to start your new project from
 scratch. This aapage gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
