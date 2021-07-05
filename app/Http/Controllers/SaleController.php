631e0d60d2c306aa8c5b776b081b70c296cf1ca4
Vaghani Vivek <77238899+vaghanivivek@users.noreply.github.com>
2021-07-05 13:00:20 -0700

    add profit loss

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -119,27 +119,28 @@ class SaleController extends Controller
 
         // Profit And Loss Formula
 
-        // $purchase_price = DB::table('products')
-        // ->select('purchaseprice')
-        // ->where('productid',$req->input('product'))
-        // ->value('purchaseprice');
+        $purchase_price = DB::table('products')
+        ->select('purchaseprice')
+        ->where('productid',$req->input('product'))
+        ->value('purchaseprice');
         
-        // $total_sale_price = $req->input('pqty') * $req->input('prate');
-        // $total_purchse_price = $req->input('pqty') * $purchase_price;
+        $total_sale_price = $req->input('pqty') * $req->input('prate');
+        $total_purchse_price = $req->input('pqty') * $purchase_price;
 
-        // $total_profit = $total_sale_price - $total_purchse_price;
+        $total_profit = $total_sale_price - $total_purchse_price;
 
-        // $pr = new Profit;
-        // $pr->profitid;
-        // $pr->saleinvoiceno = $sal->saleinvoiceno;
-        // $pr->productid = $req->input('product');
-        // $pr->saledate = $req->input('pdate');
-        // $pr->saleqty = $req->input('pqty');
-        // $pr->saleprice = $req->input('prate');
-        // $pr->totalsaleprice = $req->input('pqty') * $req->input('prate');
-        // $pr->totalpurchaseprice = $req->input('pqty') * $purchase_price;
-        // $pr->profitamount = $total_profit;
-        // $pr->save();
+        $pr = new Profit;
+        $pr->profitid;
+        $pr->saleinvoiceno = $sal->saleinvoiceno;
+        $pr->productid = $req->input('product');
+        $pr->saledate = $req->input('pdate');
+        $pr->saleqty = $req->input('pqty');
+        $pr->saleprice = $req->input('prate');
+        $pr->purchaseprice = $purchase_price;
+        $pr->totalsaleprice = $req->input('pqty') * $req->input('prate');
+        $pr->totalpurchaseprice = $req->input('pqty') * $purchase_price;
+        $pr->profitamount = $total_profit;
+        $pr->save();
 
         
         $req->session()->flash('status','Product Sale Sucessfully');
